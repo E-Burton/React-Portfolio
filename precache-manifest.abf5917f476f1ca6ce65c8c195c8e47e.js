7ef30324e97571e8f2a3686c930ab91244ecfc06
E-Burton <edwina.antoinette@gmail.com>
2021-03-08 07:01:42 -0700

    Updates

---
+++ precache-manifest.abf5917f476f1ca6ce65c8c195c8e47e.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73daadd17d4144ebea3aed1dfb9b85a",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "8b30e0258448cbea46f2",
+    "url": "/React-Portfolio/static/css/main.fedf132f.chunk.css"
+  },
+  {
+    "revision": "308dddbec719ff3582d3",
+    "url": "/React-Portfolio/static/js/2.0e21d590.chunk.js"
+  },
+  {
+    "revision": "8dbc9bd92b344a5deca62eeacc09fc2d",
+    "url": "/React-Portfolio/static/js/2.0e21d590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b30e0258448cbea46f2",
+    "url": "/React-Portfolio/static/js/main.2ca027f3.chunk.js"
+  },
+  {
+    "revision": "5dd073391d65f631c3f2",
+    "url": "/React-Portfolio/static/js/runtime-main.552ccfdd.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/React-Portfolio/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/React-Portfolio/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/React-Portfolio/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/React-Portfolio/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/React-Portfolio/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/React-Portfolio/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/React-Portfolio/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "a4a4ebdeb84358737837354e0e0f64ab",
+    "url": "/React-Portfolio/static/media/edwina.a4a4ebde.JPG"
+  },
+  {
+    "revision": "448d571051d132ecf6d02d5fdb20fac1",
+    "url": "/React-Portfolio/static/media/employee-tracker-demo.448d5710.gif"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/React-Portfolio/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/React-Portfolio/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/React-Portfolio/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "80d566111e96fd8b12de2efe55b8e988",
+    "url": "/React-Portfolio/static/media/note-taker-demo.80d56611.gif"
+  },
+  {
+    "revision": "0d55b663b9ea28fb7bdf5b414ec7df0b",
+    "url": "/React-Portfolio/static/media/password-generator-demo.0d55b663.png"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/React-Portfolio/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "db228afac5c76002a37830993168048a",
+    "url": "/React-Portfolio/static/media/pwa-budget-tracker.db228afa.png"
+  },
+  {
+    "revision": "7cc6fed9f2d778c247e72324f19cb182",
+    "url": "/React-Portfolio/static/media/quiz-demo.7cc6fed9.gif"
+  },
+  {
+    "revision": "65f21a5c1fbaf7662bd0d34e0b95924e",
+    "url": "/React-Portfolio/static/media/react-employee-directory.65f21a5c.png"
+  },
+  {
+    "revision": "40d2e6265793ab76946c0cf12458979b",
+    "url": "/React-Portfolio/static/media/readme-generator.40d2e626.gif"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/React-Portfolio/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "010e6fb8e95b0d9f0a8071f7b7413726",
+    "url": "/React-Portfolio/static/media/weather-dashboard-demo.010e6fb8.gif"
+  }
+]);
